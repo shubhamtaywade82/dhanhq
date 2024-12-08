228ec3399c90820e69c4e540972348e9fbd75ca4
Shubham Taywade <shubhamtaywade82@gmail.com>
2024-12-08 14:37:47 +0530

    Add validators for Portfolio ConvertPostions

--- lib/dhanhq/validators/portfolio/convert_position_validator.rb
+++ lib/dhanhq/validators/portfolio/convert_position_validator.rb
@@ -1,23 +1,28 @@
 # frozen_string_literal: true
 
-require "dry/validation"
+require_relative "../base_validator"
 
 module Dhanhq
   module Validators
     module Portfolio
-      # Validates the parameters required for converting a position.
-      # Ensures all fields are present and meet the API's constraints.
-      class ConvertPositionValidator < Dry::Validation::Contract
+      # Validator for converting a position
+      class ConvertPositionValidator < Dhanhq::Validators::BaseValidator
         params do
           required(:dhanClientId).filled(:string)
           required(:fromProductType).filled(:string, included_in?: %w[CNC INTRADAY MARGIN CO BO])
-          required(:toProductType).filled(:string, included_in?: %w[CNC INTRADAY MARGIN CO BO])
+          required(:exchangeSegment).filled(:string, included_in?: Dhanhq::Constants::EXCHANGE_SEGMENTS.keys)
+          required(:positionType).filled(:string, included_in?: %w[LONG SHORT CLOSED])
           required(:securityId).filled(:string)
           required(:convertQty).filled(:integer, gt?: 0)
+          required(:toProductType).filled(:string, included_in?: %w[CNC INTRADAY MARGIN CO BO])
+          optional(:tradingSymbol).maybe(:string)
         end
 
+        # Rule to ensure fromProductType and toProductType are not the same
         rule(:fromProductType, :toProductType) do
-          key.failure("must not be the same") if values[:fromProductType] == values[:toProductType]
+          if values[:fromProductType] == values[:toProductType]
+            key(:toProductType).failure("must be different from fromProductType")
+          end
         end
       end
     end
